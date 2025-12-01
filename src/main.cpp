dcf10b9193307b7be5ac8bf16ceb1ca44587d4a4
Choimoe <qwq.shq@gmail.com>
2025-12-01 13:45:25 +0800

    feat: 添加项目版本支持，更新命令行选项以显示版本信息，并在构建过程中生成 SHA256 校验和

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,28 @@
 
 void UnitTest();
 
+#ifndef PROJECT_VERSION
+#define PROJECT_VERSION "unknown"
+#endif
+
 int main(int argc, char* argv[]) {
     cxxopts::Options options("calc_server", "国标麻将 C++ 算番服务器");
     options.add_options()
         ("t,unit-test", "执行单元测试", cxxopts::value<bool>()->default_value("false"))
         ("p,port", "指定服务器端口", cxxopts::value<int>()->default_value("17711"))
         ("l,log", "指定日志文件路径", cxxopts::value<std::string>()->default_value(""))
+        ("v,version", "显示版本信息")
         ("h,help", "显示帮助信息");
 
     auto result = options.parse(argc, argv);
 
     if (result.count("help")) {
         std::cout << options.help() << std::endl;
+        return 0;
+    }
+
+    if (result.count("version")) {
+        std::cout << "calc_server version " << PROJECT_VERSION << std::endl;
         return 0;
     }
 
